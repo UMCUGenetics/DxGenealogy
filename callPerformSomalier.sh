026723df2a43b2f2b52dd2208f40989be7f98892
NI-S <43204528+NI-S@users.noreply.github.com>
2020-09-11 14:18:58 +0200

    Create callPerformSomalier.sh

---
+++ callPerformSomalier.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Call file and execute using slurm.
+
+sbatch performSomalier.sh 1
+sbatch performSomalier.sh 2
+sbatch performSomalier.sh 3
+sbatch performSomalier.sh 4
+sbatch performSomalier.sh 5
+sbatch performSomalier.sh 6
+sbatch performSomalier.sh 7
+sbatch performSomalier.sh 8
+sbatch performSomalier.sh 9
+sbatch performSomalier.sh 10
+sbatch performSomalier.sh 11
+sbatch performSomalier.sh 12
+sbatch performSomalier.sh 13
+sbatch performSomalier.sh 14
+sbatch performSomalier.sh 15
+sbatch performSomalier.sh 16
+sbatch performSomalier.sh 17
+sbatch performSomalier.sh 18
+sbatch performSomalier.sh 19
+sbatch performSomalier.sh 20
+sbatch performSomalier.sh 21
+sbatch performSomalier.sh 22
