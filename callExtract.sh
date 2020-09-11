cc6deb441b02eee1a9f72bb45bddc522bbbd2aa8
NI-S <43204528+NI-S@users.noreply.github.com>
2020-09-11 14:15:04 +0200

    Create callExtract.sh

---
+++ callExtract.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Call file and run using slurm.
+
+sbatch extractSomalier.sh 1
+sbatch extractSomalier.sh 2
+sbatch extractSomalier.sh 3
+sbatch extractSomalier.sh 4
+sbatch extractSomalier.sh 5
+sbatch extractSomalier.sh 6
+sbatch extractSomalier.sh 7
+sbatch extractSomalier.sh 9
+sbatch extractSomalier.sh 10
+sbatch extractSomalier.sh 11
+sbatch extractSomalier.sh 12
+sbatch extractSomalier.sh 13
+sbatch extractSomalier.sh 14
+sbatch extractSomalier.sh 15
+sbatch extractSomalier.sh 16
+sbatch extractSomalier.sh 17
+sbatch extractSomalier.sh 18
+sbatch extractSomalier.sh 19
+sbatch extractSomalier.sh 20
+sbatch extractSomalier.sh 21
+sbatch extractSomalier.sh 22
