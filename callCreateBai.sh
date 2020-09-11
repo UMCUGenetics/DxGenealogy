653bda2a65bb3d5fb6356ae562675bbcd8ec7288
NI-S <43204528+NI-S@users.noreply.github.com>
2020-09-11 14:05:57 +0200

    Create callCreateBai.sh

---
+++ callCreateBai.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Calling file and perform using slurm.
+
+sbatch createBai.sh 1
+sbatch createBai.sh 2
+sbatch createBai.sh 3
+sbatch createBai.sh 4
+sbatch createBai.sh 5
+sbatch createBai.sh 6
+sbatch createBai.sh 7
+sbatch createBai.sh 8
+sbatch createBai.sh 9
+sbatch createBai.sh 10
+sbatch createBai.sh 11
+sbatch createBai.sh 12
+sbatch createBai.sh 13
+sbatch createBai.sh 14
+sbatch createBai.sh 15
+sbatch createBai.sh 16
+sbatch createBai.sh 17
+sbatch createBai.sh 18
+sbatch createBai.sh 19
+sbatch createBai.sh 20
+sbatch createBai.sh 21
+sbatch createBai.sh 22
