68c047622ff92ddf1f84ade3a69facc6048d8b39
NI-S <43204528+NI-S@users.noreply.github.com>
2020-11-13 12:47:10 +0100

    Update and rename IBD.R to IBD.Rmd

--- IBD.R
+++
@@ -1,7 +0,0 @@
-args<-commandArgs(TRUE)
-file <- read.table(args[1], header=FALSE, sep="\t")
-#file
-file <- file[,c(1,3,5,6,7)]
-file
-colnames(file) <- c("Sample1","Sample2","Chromosome", "Start", "End")
-file
